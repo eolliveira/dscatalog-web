73e7f377550a64d2aa81fc366ca618dee826ea3c
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-09-28 00:32:49 -0300

    Catalog Listing

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,27 +1,35 @@
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Pagination from '../../components/Pagination';
 import ProductCard from '../../components/ProductCard';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { Product } from '../../types/Product';
+import { SpringPage } from '../../types/vendor/spring';
+import { AxiosParams } from '../../types/vendor/axios';
+import { baseUrl } from '../../http/requests';
+
 import './style.css';
 
 const Catalog = () => {
-  const product = {
-    id: 1,
-    name: 'The Lord of the Rings',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 1190.5,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
-    date: '2020-07-13T20:50:07.123450Z',
-    categories: [
-      {
-        id: 2,
-        name: 'Eletrônicos',
-        products: [],
+  //estado do tipo page de Product
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${baseUrl}/products`,
+      //data = corpo da req
+      params: {
+        page: 0,
+        size: 12,
       },
-    ],
-  };
+    };
+
+    axios(params).then((response) => {
+      setPage(response.data);
+      console.log(response.data);
+    });
+  }, []);
 
   return (
     <div className="container catalog-container">
@@ -30,31 +38,14 @@ const Catalog = () => {
       </div>
 
       <div className="row">
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+        {page?.content.map(product => (
+          <div className="col-sm-6 col-md-4 col-lg-3" key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              {/* deve-se informar uma chave unica para os elemento de uma coleção */}
+              <ProductCard product={product} />
+            </Link>
+          </div>
+        ))}
       </div>
 
       <div className="row catalog-pagination">
---
+++ src/types/vendor/spring.ts
@@ -0,0 +1,13 @@
+//tipo genérico de uma resposta(Page) do Spring
+export type SpringPage<T> = {
+    content: T[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty: boolean;
+  };
+  
