02ac90c3da9a8774af305af8a9d8cb3d5f35c93e
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-10-10 21:36:26 -0300

    Adicionando React Hook Form na tela de Login

--- src/pages/Admin/Auth/Login/index.tsx
+++ src/pages/Admin/Auth/Login/index.tsx
@@ -1,15 +1,38 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from '../../../../components/ButtonIcon';
+import { useForm } from 'react-hook-form';
 
 import './style.css';
- 
+
 const Login = () => {
+  //tipo dos dados do formulário
+  type FormData = {
+    username: string;
+    password: string;
+  };
+
+
+  //register = registra os inputs dos formulário
+  //handleSubmit = evento do submit do formulário
+
+
+  //hook para monitorar o formulário
+  const { register, handleSubmit } = useForm<FormData>();
+
+  //recebe o tipo do formulário
+  const onSubmit = (formData: FormData) => {
+    console.log(formData);
+  };
+
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      <form>
+      {/* evento do submit sendo monitorado pelo evento do hook */}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
+            //registra input para ser monitorado pelo hook(deve ser igual ao name)
+            {...register('username')}
             type="text"
             className="form-control base-input"
             placeholder="Email"
@@ -18,6 +41,7 @@ const Login = () => {
         </div>
         <div className="mb-2">
           <input
+            {...register('password')}
             type="password"
             className="form-control base-input "
             placeholder="Password"
@@ -27,9 +51,9 @@ const Login = () => {
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
         </Link>
-        <div className="login-submit">
-          <ButtonIcon text="Fazer login" />
-        </div>
+        <button className="login-submit">
+          <ButtonIcon  text="Fazer login" />
+        </button>
         <div className="signup-container">
           <span className="not-registered">Não tem Cadastro?</span>
           <Link to="/admin/auth/register" className="login-link-register">
@@ -40,5 +64,5 @@ const Login = () => {
     </div>
   );
 };
- 
+
 export default Login;
