18c04d69d6dc342d6bb5cef70c40a2b197978da7
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-10-23 18:08:21 -0300

    hasAnyRoles

--- src/http/requests.ts
+++ src/http/requests.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import jwtDecode from 'jwt-decode';
 import qs from 'qs';
+import { Role } from '../types/Role';
 import { TokenData } from '../types/TokenData';
 import history from '../util/history';
 
@@ -69,7 +70,6 @@ export const removeAuthData = () => {
   localStorage.removeItem('authKey');
 };
 
-
 export const requestBackend = (config: AxiosRequestConfig) => {
   //se solicitar autenticação
   const headers = config.withCredentials
@@ -117,18 +117,39 @@ axios.interceptors.response.use(
 
 //metodo para decodificar token e devolve obj tokenData(pode retornar o tipo, ou undefined)
 export const getTokenData = (): TokenData | undefined => {
-  try{
+  try {
     return jwtDecode(getAuthData().access_token) as TokenData;
-  }
-  catch(error) {
+  } catch (error) {
     return undefined;
   }
 };
 
-
 //função que identifica se o usuário esta autênticado
-export const isAuthenticated = () : boolean => {
-  const tokenData = getTokenData()
+export const isAuthenticated = (): boolean => {
+  const tokenData = getTokenData();
   //verifica se tokenData não é undefined e se o token não esta expirado
-  return (tokenData && tokenData?.exp * 1000 > Date.now()) ? true : false;
-}
+  return tokenData && tokenData?.exp * 1000 > Date.now() ? true : false;
+};
+
+//função que verifica se usuário, possui alguem role da lista de  parametros
+export const hasAnyHole = (roles: Role[]): boolean => {
+  if (roles.length === 0) return true;
+
+  //captura obj com os dados do usuário
+  const tokenData = getTokenData();
+
+  if (tokenData !== undefined) {
+    //returna true se atender ao predicado
+    return roles.some((role) => tokenData.authorities.includes(role));
+
+    //metodo alternativo
+    // for (var i = 0; i < roles.length; i++) {
+    //   //percorre lista Roles do usuário , se contem algum da lista de Role que veio no parametro
+    //   if (tokenData.authorities.includes(roles[i])) {
+    //     return true;
+    //   }
+    // }
+  }
+
+  return false;
+};
