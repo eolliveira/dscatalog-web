0de7fb207ce0032283e916848c7bf4c651987cec
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-10-23 18:09:43 -0300

    hasAnyRoles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import './style.css';
 import { ReactComponent as MainImage } from '../../assets/img/main-image.svg';
 import ButtonIcon from '../../components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { isAuthenticated } from '../../http/requests';
+import { hasAnyHole, isAuthenticated } from '../../http/requests';
 
 const Home = () => {
   return (
@@ -12,7 +12,10 @@ const Home = () => {
 
           {/* teste */}
           {isAuthenticated() ? 'esta autenticado' : 'não autenticado'}
-        
+
+          {/* passa lista de Roles e verifica se o usuário logado as pertence */}
+          { <h1>Resultado: { hasAnyHole(['ROLE_ADMIN']) ? 'sim' : 'não' }</h1> }
+
           <div className="home-card-content-container">
             <h1>Conheça o melhor catálogo de produtos</h1>
             <p>
